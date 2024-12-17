aa90affb9e57e6b08714fba86ffd369a2a3b3dd8
ccoalman <2732529@sd61learn.ca>
2024-12-17 10:29:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ window.onload = function() {
   fetchLatestEpisodes();
 }
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}  
+
 async function searchTvShows() {
   document.getElementById("main").innerHTML = "";
   document.getElementById("episodes-list").innerHTML = ""; 
@@ -151,7 +161,7 @@ async function epInfo(id) {
 
 async function fetchLatestEpisodes() {
   try {
-    const response = await fetch('https://api.tvmaze.com/schedule');
+    const response = await fetch('http://api.tvmaze.com/schedule');
     const episodes = await response.json();
     if (episodes.length > 0) {
       document.getElementById("latest-episodes").style.display = "block";
@@ -174,12 +184,27 @@ async function fetchLatestEpisodes() {
   }
 }
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }, function(error) {
-      console.log('Service Worker registration failed:', error);
+
+// handle install prompt
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
     });
   });
-}  
+});   
